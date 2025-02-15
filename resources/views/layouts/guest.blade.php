fea0cc9500ea57562238170334af433d66abdfc9
Nguyễn Từ Thành Đạt <nttd0801@gmail.com>
2025-02-15 10:43:03 +0700

    fix link to vite

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,10 +19,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
 
     <!-- Scripts -->
-    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-d96bec99.css') }}">
-    <script src="{{ secure_asset('build/assets/app-226659d9.js') }}"></script>
-   
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
