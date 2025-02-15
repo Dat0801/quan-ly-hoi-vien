464d4360ee124a5ae0889c49a0a469dc6b7ccd1c
Nguyễn Từ Thành Đạt <nttd0801@gmail.com>
2025-02-15 09:10:30 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,26 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
   plugins: [
-    laravel([
-      'resources/css/app.css',
-      'resources/js/app.js',
-    ]),
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
   ],
+  build: {
+    outDir: 'public/build',
+    manifest: true,
+    rollupOptions: {
+      input: {
+        app: 'resources/js/app.js',
+        styles: 'resources/css/app.css', 
+      },
+    },
+  },
   server: {
+    host: '0.0.0.0',
     https: true,
-    host: '0.0.0.0'
-  }
+  },
 });
-
