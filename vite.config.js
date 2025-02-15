b70f94cebe5deba1b59dff1e248b8b5b58c0fb7e
Nguyễn Từ Thành Đạt <nttd0801@gmail.com>
2025-02-15 08:17:00 +0700

    Fix: include public/build

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,9 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
   plugins: [
-    laravel({
-      input: [
-        'resources/css/app.css',
-        'resources/js/app.js',
-      ],
-      refresh: true,
-    }),
+    laravel([
+      'resources/css/app.css',
+      'resources/js/app.js',
+    ]),
   ],
-  build: {
-    outDir: 'public/build',  
-    manifest: true,          
-    rollupOptions: {
-      input: 'resources/js/app.js',  
-    },
-  },
 });
